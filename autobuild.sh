76787b40ee9c86df0c3b0d38e691921738c3c084
HF <2625914411@qq.com>
2025-04-23 09:50:38 +0800

    [ADD]add new branch

--- autobuild.sh
+++ autobuild.sh
@@ -3,6 +3,7 @@
 set -e
 
 rm -rf `pwd`/build/*
+mkdir -p `pwd`/build
 cd `pwd`/build &&
 	cmake .. &&
 	make
---
+++ build/example/callee/CMakeFiles/f_provider.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hf/CPPProjects/MPRPC/mprpc/example/friend.pb.cc" "example/callee/CMakeFiles/f_provider.dir/__/friend.pb.cc.o" "gcc" "example/callee/CMakeFiles/f_provider.dir/__/friend.pb.cc.o.d"
+  "/home/hf/CPPProjects/MPRPC/mprpc/example/publictype.pb.cc" "example/callee/CMakeFiles/f_provider.dir/__/publictype.pb.cc.o" "gcc" "example/callee/CMakeFiles/f_provider.dir/__/publictype.pb.cc.o.d"
+  "/home/hf/CPPProjects/MPRPC/mprpc/example/callee/friendservice.cc" "example/callee/CMakeFiles/f_provider.dir/friendservice.cc.o" "gcc" "example/callee/CMakeFiles/f_provider.dir/friendservice.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/hf/CPPProjects/MPRPC/mprpc/build/src/CMakeFiles/mprpc.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
